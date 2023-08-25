5fec60a3fb554207536e63dca72d631c33b08fc5
Anshuman550 <80764089+Anshuman550@users.noreply.github.com>
2023-08-25 17:19:20 +0530

    Add files via upload

---
+++ frontend/src/lib/auth.js
@@ -0,0 +1,15 @@
+const user_is_authenticated = () => {   
+    console.log(localStorage.getItem('token'))                // check login, different pages displayed for logged in users
+    return localStorage.getItem('token');
+}
+
+const user_logout = () => {                         // Delete tokens on logout
+    localStorage.removeItem('token');
+    localStorage.removeItem('user_type');
+}
+
+const user_type = () => {                           // checking whether the user is a buyer or vendor
+    return localStorage.getItem('user_type');
+}
+
+export { user_is_authenticated, user_type, user_logout };
